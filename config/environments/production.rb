5345925b856e168d4cc2897e0fcb4fd5f974aa0b
Adam <adammoore118@outlook.com>
2022-02-28 16:10:31 -0500

    test7

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -13,7 +13,8 @@ Rails.application.configure do
   config.eager_load = true
 
   # Full error reports are disabled and caching is turned on.
-  config.consider_all_requests_local       = true
+  config.consider_all_requests_local       = false
+  config.action_dispatch.cookies_same_site_protection = :None
 
   # Ensures that a master key has been made available in either ENV["RAILS_MASTER_KEY"]
   # or in config/master.key. This key is used to decrypt credentials (and other encrypted files).
