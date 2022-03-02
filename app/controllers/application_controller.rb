96b140e7d90051044f6da833e9221fd488913c04
Adam <adammoore118@outlook.com>
2022-03-02 17:56:57 -0500

    test17

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,7 +10,7 @@ class ApplicationController < ActionController::API
 
 
     def current_user
-     @current_user = User.find(session[:user_id])
+     @current_user = User.find_by(id: session[:user_id])
     
     end
 
