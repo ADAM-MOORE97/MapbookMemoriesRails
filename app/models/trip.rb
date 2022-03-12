6276e00c9a80a307a1bfffbe77e1548a67d4b31e
Adam <adammoore118@outlook.com>
2022-03-11 19:25:16 -0500

    resize6

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -2,7 +2,9 @@ class Trip < ApplicationRecord
  
     belongs_to :user 
     belongs_to :location
-    has_many_attached :attachments
+    has_many_attached :attachments.each do |image|
+        image.variatn(resize_to_limit: [200,200])
+    end
     validates :name, uniqueness: true
     validates :name, presence: true
     validates :start_date, presence: true
