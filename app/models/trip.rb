56bfb7e9ce8738432fd043ba0beb701f395121a2
Adam <adammoore118@outlook.com>
2022-03-11 19:36:53 -0500

    backend fully functional with cookies and images

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -2,9 +2,7 @@ class Trip < ApplicationRecord
  
     belongs_to :user 
     belongs_to :location
-    has_many_attached :attachments.each do |image|
-        image.variant(resize_to_limit: [200,200])
-    end
+    has_many_attached :attachments
     validates :name, uniqueness: true
     validates :name, presence: true
     validates :start_date, presence: true
@@ -15,10 +13,9 @@ class Trip < ApplicationRecord
     def attachment_urls
         attachments.map do |attached|
   Rails.application.routes.url_helpers.rails_blob_path(attached, only_path: true)
-#  attached.url
+
             
         end
     end
  
 end
-# Rails.application.routes.url_helpers.rails_blob_path(attached.variant(resize_to_limit: [200,200]), only_path: true)
